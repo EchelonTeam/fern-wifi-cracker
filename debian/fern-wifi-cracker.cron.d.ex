17873ae72586fea63d1dc952d2831dc38a885dac
Devon Kearns <dookie@kali.org>
2013-01-14 07:46:27 -0700

    Pre-build commit

--- debian/fern-wifi-cracker.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the fern-wifi-cracker package
-#
-0 4	* * *	root	[ -x /usr/bin/fern-wifi-cracker_maintenance ] && /usr/bin/fern-wifi-cracker_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(fern-wifi-cracker):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="fern-wifi-cracker" command="/usr/bin/fern-wifi-cracker"

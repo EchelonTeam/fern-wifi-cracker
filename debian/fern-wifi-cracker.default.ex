c85eac0ad8e899a92585f77a38684d498e28be82
Devon Kearns <dookie@kali.org>
2013-01-14 07:32:21 -0700

    Initial commit

---
+++ debian/fern-wifi-cracker.default.ex
@@ -0,0 +1,10 @@
+# Defaults for fern-wifi-cracker initscript
+# sourced by /etc/init.d/fern-wifi-cracker
+# installed at /etc/default/fern-wifi-cracker by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(fern-wifi-cracker):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="fern-wifi-cracker" command="/usr/bin/fern-wifi-cracker"
